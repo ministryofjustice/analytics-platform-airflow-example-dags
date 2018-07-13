a61303a4d3a48d021e50f4c6a360918250cdb864
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-07-13 21:04:33 +0100

    try with explicit bash cmd

--- example_dags/occupeye_scraper.py
+++ example_dags/occupeye_scraper.py
@@ -18,8 +18,8 @@ try:
     k = KubernetesPodOperator(
         namespace="airflow",
         image="robinlinacre/airflow-occupeye-scraper:firsttry",
-        cmds=["python main.py --task_name surveys_to_s3"],
-        arguments=[""],
+        cmds=["bash -c"],
+        arguments=["python main.py --task_name surveys_to_s3"],
         labels={"foo": "bar"},
         name="airflow-test-pod",
         in_cluster=True,
