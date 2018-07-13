09439fcaafac5494937a457d37d015e43e99cf5e
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-07-13 19:32:17 +0100

    added occupeye scraper

---
+++ example_dags/occupeye_scraper.py
@@ -0,0 +1,34 @@
+from airflow.utils.dates import days_ago
+from airflow.utils.log.logging_mixin import LoggingMixin
+from airflow.models import DAG
+
+log = LoggingMixin().log
+
+try:
+    from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+
+    args = {"owner": "airflow", "start_date": days_ago(1)}
+
+    dag = DAG(
+        dag_id="example_kubernetes_operator_assume_role",
+        default_args=args,
+        schedule_interval=None,
+    )
+
+    k = KubernetesPodOperator(
+        namespace="airflow",
+        image="robinlinacre/airflow-occupeye-scraper:firsttry",
+        cmds=["],
+        arguments=["python main.py --myoption myarg""],
+        labels={"foo": "bar"},
+        name="airflow-test-pod",
+        in_cluster=True,
+        task_id="task",
+        get_logs=True,
+        dag=dag,
+        annotations={"iam.amazonaws.com/role": "dev_ravi_test_airflow_assume_role"},
+    )
+
+
+except ImportError as e:
+    log.warn("Could not import KubernetesPodOperator: " + str(e))
