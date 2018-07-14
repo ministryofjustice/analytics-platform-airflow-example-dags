3a941728c4349f5613681d9f8a07ebf901b1f95d
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-07-14 16:13:49 +0100

    revert

--- example_dags/occupeye_scraper.py
+++ example_dags/occupeye_scraper.py
@@ -15,20 +15,20 @@ try:
         schedule_interval=None,
     )
 
-    surveys_to_xcom = KubernetesPodOperator(
-        namespace="airflow",
-        image="robinlinacre/airflow-occupeye-scraper:second",
-        cmds=["bash", "-c"],
-        arguments=["python main.py --task_name surveys_to_xcom"],
-        labels={"foo": "bar"},
-        name="airflow-test-pod",
-        in_cluster=True,
-        task_id="surveys_to_xcom",
-        get_logs=True,
-        dag=dag,
-        annotations={"iam.amazonaws.com/role": "dev_ravi_test_airflow_assume_role"},
-        xcom_push=True
-    )  #Can't annotate so won't have s3 perm
+    # surveys_to_xcom = KubernetesPodOperator(
+    #     namespace="airflow",
+    #     image="robinlinacre/airflow-occupeye-scraper:second",
+    #     cmds=["bash", "-c"],
+    #     arguments=["python main.py --task_name surveys_to_xcom"],
+    #     labels={"foo": "bar"},
+    #     name="airflow-test-pod",
+    #     in_cluster=True,
+    #     task_id="surveys_to_xcom",
+    #     get_logs=True,
+    #     dag=dag,
+    #     annotations={"iam.amazonaws.com/role": "dev_ravi_test_airflow_assume_role"},
+    #     xcom_push=True
+    # )  #Can't annotate so won't have s3 perm
 
     surveys_to_s3 = KubernetesPodOperator(
         namespace="airflow",
@@ -44,7 +44,9 @@ try:
         annotations={"iam.amazonaws.com/role": "dev_ravi_test_airflow_assume_role"},
     )
 
-    surveys_to_xcom >> surveys_to_s3
+    # surveys_to_xcom >> surveys_to_s3
+
+    # surveys_to_s3
 
     # v1 = surveys_to_xcom.xcom_pull(key='python_set', task_ids='push')
 
