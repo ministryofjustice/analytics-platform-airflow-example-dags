8cd8ff949f40e938165e6462b2ec110cfc5fecbe
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-07-14 16:24:55 +0100

    try xcom only

--- example_dags/occupeye_scraper.py
+++ example_dags/occupeye_scraper.py
@@ -15,34 +15,33 @@ try:
         schedule_interval=None,
     )
 
-    # surveys_to_xcom = KubernetesPodOperator(
-    #     namespace="airflow",
-    #     image="robinlinacre/airflow-occupeye-scraper:second",
-    #     cmds=["bash", "-c"],
-    #     arguments=["python main.py --task_name surveys_to_xcom"],
-    #     labels={"foo": "bar"},
-    #     name="airflow-test-pod",
-    #     in_cluster=True,
-    #     task_id="surveys_to_xcom",
-    #     get_logs=True,
-    #     dag=dag,
-    #     annotations={"iam.amazonaws.com/role": "dev_ravi_test_airflow_assume_role"},
-    #     xcom_push=True
-    # )  #Can't annotate so won't have s3 perm
-
-    surveys_to_s3 = KubernetesPodOperator(
+    surveys_to_xcom = KubernetesPodOperator(
         namespace="airflow",
         image="robinlinacre/airflow-occupeye-scraper:second",
         cmds=["bash", "-c"],
-        arguments=["python main.py --task_name surveys_to_s3"],
+        arguments=["python main.py --task_name surveys_to_xcom"],
         labels={"foo": "bar"},
         name="airflow-test-pod",
         in_cluster=True,
-        task_id="surveys_to_s3",
+        task_id="surveys_to_xcom",
         get_logs=True,
         dag=dag,
-        annotations={"iam.amazonaws.com/role": "dev_ravi_test_airflow_assume_role"},
-    )
+        annotations={"iam.amazonaws.com/role": "dev_ravi_test_airflow_assume_role"}
+    )  #Can't annotate so won't have s3 perm
+
+    # surveys_to_s3 = KubernetesPodOperator(
+    #     namespace="airflow",
+    #     image="robinlinacre/airflow-occupeye-scraper:second",
+    #     cmds=["bash", "-c"],
+    #     arguments=["python main.py --task_name surveys_to_s3"],
+    #     labels={"foo": "bar"},
+    #     name="airflow-test-pod",
+    #     in_cluster=True,
+    #     task_id="surveys_to_s3",
+    #     get_logs=True,
+    #     dag=dag,
+    #     annotations={"iam.amazonaws.com/role": "dev_ravi_test_airflow_assume_role"},
+    # )
 
     # surveys_to_xcom >> surveys_to_s3
 
