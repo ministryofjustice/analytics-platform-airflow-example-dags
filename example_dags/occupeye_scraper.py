bb25b052a517b01e6e830c56a972901b11b045c1
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-07-14 16:30:10 +0100

    try xcom push

--- example_dags/occupeye_scraper.py
+++ example_dags/occupeye_scraper.py
@@ -26,7 +26,8 @@ try:
         task_id="surveys_to_xcom",
         get_logs=True,
         dag=dag,
-        annotations={"iam.amazonaws.com/role": "dev_ravi_test_airflow_assume_role"}
+        annotations={"iam.amazonaws.com/role": "dev_ravi_test_airflow_assume_role"},
+        xcom_push=True
     )  #Can't annotate so won't have s3 perm
 
     # surveys_to_s3 = KubernetesPodOperator(
