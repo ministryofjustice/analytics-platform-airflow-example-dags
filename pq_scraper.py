1b694e34dbbe4566bbeb1c90e66ecb6c8fa77236
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2018-07-18 15:57:51 +0100

    More pq_scraper fixes

--- pq_scraper.py
+++ pq_scraper.py
@@ -6,64 +6,79 @@ from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOpera
 from airflow.utils.dates import days_ago
 
 
-PQ_SCRAPER_IMAGE = "quay.io/mojanalytics/pq_scraper:v0.1.2"
-PQ_SCRAPER_IAM_ROLE = "dev_pq_scraper_dag"
-UPDATE_LAST_N_DAYS = 5
+SCRAPER_IMAGE = "quay.io/mojanalytics/pq_scraper:v0.1.2"
+SCRAPER_IAM_ROLE = "dev_pq_scraper_dag"
+SCRAPER_S3_BUCKET = "dev-aldo-test-20170927-1505"
+SCRAPER_S3_OBJECT_PREFIX = "pq_raw/answered_questions_"
 
-default_args = {
-    "owner": "xoen",
-    "email": ["aldogiambelluca@digital.justice.gov.uk"],
-    "schedule_interval": timedelta(days=1),
+CATCHUP_START = datetime(2018, 2, 1)
+REUPDATE_LAST_N_DAYS = 31
+
+
+task_args = {
     "depends_on_past": False,
     "email_on_failure": True,
     "email_on_retry": True,
-    "retries": 5,
-    "retry_delay": timedelta(minutes=5),
+    "retries": 15,
+    "retry_delay": timedelta(seconds=30),
+    "retry_exponential_backoff": True,
+    "max_retry_delay": timedelta(minutes=15),
+    "execution_timeout": timedelta(minutes=10),
+    "owner": "xoen",
+    "email": ["aldogiambelluca@digital.justice.gov.uk"],
 }
 
-# Catch-up on dates before today-UPDATE_LAST_N_DAYS days
+# Catch-up on dates before today-REUPDATE_LAST_N_DAYS days
 dag_catchup = DAG(
     "pq_scraper_catchup",
-    default_args={
-        **default_args,
-        "start_date": datetime(2018, 6, 1),
-        "end_date": days_ago(UPDATE_LAST_N_DAYS + 1),
-    },
+    default_args=task_args,
     description="Get answered parliamentary questions (PQ) from parliament.uk API",
+    start_date=CATCHUP_START,
+    end_date=days_ago(REUPDATE_LAST_N_DAYS - 1),
+    schedule_interval=timedelta(days=1),
 )
 
 task_catchup = KubernetesPodOperator(
     dag=dag_catchup,
     namespace="airflow",
-    image=PQ_SCRAPER_IMAGE,
+    image=SCRAPER_IMAGE,
+    env_vars={
+        "SCRAPER_S3_BUCKET": SCRAPER_S3_BUCKET,
+        "SCRAPER_S3_OBJECT_PREFIX": SCRAPER_S3_OBJECT_PREFIX,
+    },
     arguments=["{{ ds }}"],
-    labels={"app": dag.dag_id},
-    name="pq_scraper_catchup",
+    labels={"app": dag_catchup.dag_id},
+    name="pq-scraper-catchup",
     in_cluster=True,
-    task_id="scrape_{{ ds }}",
+    task_id="pq_scrape_catchup",
     get_logs=True,
-    annotations={"iam.amazonaws.com/role": PQ_SCRAPER_IAM_ROLE},
+    annotations={"iam.amazonaws.com/role": SCRAPER_IAM_ROLE},
 )
 
-# Update last UPDATE_LAST_N_DAYS days
-
+# Update last REUPDATE_LAST_N_DAYS days
 dag_recent_pqs = DAG(
     "pq_scraper_recent",
-    default_args={**default_args, "start_date": days_ago(UPDATE_LAST_N_DAYS)},
+    start_date=days_ago(1),
+    schedule_interval=timedelta(days=1),
+    default_args=task_args,
     description="Get answered parliamentary questions (PQ) from parliament.uk API",
 )
 
-for days_ago in range(1, UPDATE_LAST_N_DAYS):
+for days_ago in range(0, REUPDATE_LAST_N_DAYS + 1):
     KubernetesPodOperator(
         dag=dag_recent_pqs,
         namespace="airflow",
-        image=PQ_SCRAPER_IMAGE,
+        image=SCRAPER_IMAGE,
+        env_vars={
+            "SCRAPER_S3_BUCKET": SCRAPER_S3_BUCKET,
+            "SCRAPER_S3_OBJECT_PREFIX": SCRAPER_S3_OBJECT_PREFIX,
+        },
+        arguments=["{{ macros.ds_add(ds, -params.days_ago) }}"],
         params={"days_ago": days_ago},
-        arguments=["{{ ds_add(ds, -params.days_ago) }}"],
-        labels={"app": dag.dag_id},
-        name="pq_scraper_catchup",
+        labels={"app": dag_recent_pqs.dag_id},
+        name="pq-scraper-catchup",
         in_cluster=True,
-        task_id="scrape_{{ ds_add(ds, -params.days_ago) }}",
+        task_id=f"pq_scrape_recent_{days_ago}_days_ago",
         get_logs=True,
-        annotations={"iam.amazonaws.com/role": PQ_SCRAPER_IAM_ROLE},
+        annotations={"iam.amazonaws.com/role": SCRAPER_IAM_ROLE},
     )
