c36646b9b51702dcc91854e1d69eb3799f5917c5
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-10-14 16:54:29 +0100

    add occupeye aggregator

---
+++ occupeye_aggregator.py
@@ -0,0 +1,43 @@
+from airflow.utils.dates import days_ago
+from airflow.utils.log.logging_mixin import LoggingMixin
+from airflow.models import DAG
+from datetime import datetime, timedelta
+
+log = LoggingMixin().log
+
+try:
+    from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+
+    args = {"owner": "Robin",
+            "start_date": datetime(2018, 10, 14),
+            "retries": 2,
+            "retry_delay": timedelta(minutes=50),
+            "email": ["robin.linacre@digital.justice.gov.uk"],
+            "pool": "occupeye_pool"}
+
+    dag = DAG(
+        dag_id="occupeye_aggregator",
+        default_args=args,
+        schedule_interval='0 5 * * *',
+    )
+
+    surveys_to_s3 = KubernetesPodOperator(
+        namespace="airflow",
+        image="quay.io/mojanalyticsairflow-occupeye-dashboard-aggregation:dockerise",
+        cmds=["bash", "-c"],
+        arguments=["Rscript main.R"],
+        labels={"foo": "bar"},
+        name="airflow-test-pod",
+        in_cluster=True,
+        task_id="scrape_all",
+        get_logs=True,
+        dag=dag,
+        annotations={"iam.amazonaws.com/role": "alpha_airflow_occupeye_aggregator"},
+        image_pull_policy='Always'
+    )
+
+
+
+except ImportError as e:
+    log.warn("Could not import KubernetesPodOperator: " + str(e))
+
